9b94a91b69111d2c6984441871196d1128886eda
Maxim Davydov <norm.pochta.maxa@gmail.com>
2024-10-22 00:27:26 +0300

    FE-39: Добавил константы дизайна

---
+++ app/src/main/java/com/example/gorodbezproblem/ui/theme/UIConstants.kt
@@ -0,0 +1,12 @@
+package com.example.gorodbezproblem.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class UIConstants {
+    companion object {
+        val Padding = (12.5).dp
+        val Round = 13.dp
+        val FontSize = 16.sp
+    }
+}
