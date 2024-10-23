f96718177b23afbcb53b0a89b12562c45efabbfc
Maxim Davydov <norm.pochta.maxa@gmail.com>
2024-10-23 10:56:08 +0300

    FE-43: Замокал координаты проблемы

--- app/src/main/java/com/example/gorodbezproblem/models/Problem.kt
+++ app/src/main/java/com/example/gorodbezproblem/models/Problem.kt
@@ -4,5 +4,7 @@ data class Problem(
     val title: String = "",
     val description: String = "",
     val address: String = "",
-    val status: String = ""
+    val status: String = "",
+    val lat: String = "55.765903",
+    val long: String = "37.685013",
 )
