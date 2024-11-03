8b395cbffb8e423c7030ffb672a81bcfd5193b83
Maxim Davydov <norm.pochta.maxa@gmail.com>
2024-11-03 18:14:17 +0300

    FE-46: Обновил походы в API за списком проблем

--- app/src/main/java/com/example/gorodbezproblem/models/Problem.kt
+++ app/src/main/java/com/example/gorodbezproblem/models/Problem.kt
@@ -1,10 +1,14 @@
 package com.example.gorodbezproblem.models
 
 data class Problem(
+    val id: Int = 0,
     val title: String = "",
     val description: String = "",
+    val specificlocation: String = "",
+    val category: String = "cleaning",
+    val voteCount: Int = 1,
     val address: String = "",
-    val status: String = "",
+    val status: String = "AWAITING",
     val lat: String = "55.765903",
     val long: String = "37.685013",
 )
--- app/src/main/java/com/example/gorodbezproblem/models/api/ProblemService.kt
+++ app/src/main/java/com/example/gorodbezproblem/models/api/ProblemService.kt
@@ -6,9 +6,18 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 
+data class APIResponse<T, E>(
+    val Body: T? = null,
+    val Error: E? = null
+)
+
+data class ProblemsListResponse(
+    val problems: List<Problem>
+)
+
 interface ProblemService {
     @GET("/problems")
-    suspend fun getProblems() : List<Problem>
+    suspend fun getProblems() : APIResponse<ProblemsListResponse, Any>
 
     @GET("/problems/{id}")
     suspend fun getProblem(@Path("id") problemId: Int): Problem
